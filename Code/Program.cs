245544af523be97ef568a4f5c45f84431d85aa1d
Sergei Blockiy <sergei.blockiy@gmail.com>
2023-05-24 12:12:55 +0300

    add code

---
+++ Code/Program.cs
@@ -0,0 +1,48 @@
+﻿using Newtonsoft.Json;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        // Загрузка имен из файла JSON / Получилось только через абсолютный путь, через другие пути не получилось
+        List<string> names = LoadNamesFromJson("/Users/sergei/RiderProjects/ConsoleApp3/ConsoleApp3/Code/Names.json");
+
+        // Имена, заканчивающиеся на 's'
+        List<string> namesEndingWithS = names.FindAll(name => name.EndsWith("s", StringComparison.OrdinalIgnoreCase));
+        Console.WriteLine("Имена, заканчивающиеся на 's':");
+        foreach (string name in namesEndingWithS)
+        {
+            Console.WriteLine(name);
+        }
+
+        // Самое длинное имя и его размер
+        var longestName = names.MaxBy(name => name.Length);
+        Console.WriteLine("Самое длинное имя: " + longestName);
+        Console.WriteLine("Размер самого длинного имени: " + longestName.Length);
+    }
+
+    static List<string> LoadNamesFromJson(string filePath)
+    {
+        List<string> names = new List<string>();
+
+        try
+        {
+            string json = File.ReadAllText(filePath);
+            dynamic data = JsonConvert.DeserializeObject(json);
+            foreach (var name in data.names)
+            {
+                names.Add(name.ToString());
+            }
+        }
+        catch (FileNotFoundException)
+        {
+            Console.WriteLine("Файл не найден: " + filePath);
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine("Ошибка при загрузке имен из файла: " + ex.Message);
+        }
+
+        return names;
+    }
+}
